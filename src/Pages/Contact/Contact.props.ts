26208b04a4fd9ce580b29b52a6ef4792416515db
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-04-06 14:40:36 -0700

    feat: create form page cmoponent

--- src/Pages/Contact/Contact.props.ts
+++ src/Pages/Contact/Contact.props.ts
@@ -1,3 +1,3 @@
-export interface ContactForm{
+export interface ContactFormProps{
     
 }
--- src/Pages/Contact/index.tsx
+++ src/Pages/Contact/index.tsx
@@ -1,9 +1,18 @@
 import './Contact.css';
-import { ContactForm } from './Contact.props';
+import { ContactFormProps } from './Contact.props';
+import ContactForm from '../Home/Components/Contact';
+import Footer from '../../Components/Footer';
+import Header from '../../Components/Header';
 
-const Contact: React.FC<ContactForm> = ()=>{
+const Contact: React.FC<ContactFormProps> = ()=>{
     return <div className='contact'>
-        Contact works!
+        <div className='contact__header'>
+            <h1>¿Necesitas ayuda? Contáctanos y te brindaremos asistencia personalizada</h1>
+            <Header showNavigation/>
+            <img src="https://res.cloudinary.com/dvdmz9djk/image/upload/v1711558391/JOYAT/images/vgjnh5kfbvre9pysgpmp.jpg" alt="" />
+        </div>
+        <ContactForm/>
+        <Footer/>
     </div>
 }
 
