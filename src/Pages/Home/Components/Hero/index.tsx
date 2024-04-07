0546caaeff9c736766fbb17f5b2247e7f99f03e2
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-04-07 13:16:24 -0700

    style: change hero background image

--- src/Pages/Home/Components/Hero/index.tsx
+++ src/Pages/Home/Components/Hero/index.tsx
@@ -7,7 +7,7 @@ import {Select} from 'antd';
 const Hero: React.FC<HeroProps> = ()=>{
     return <div className="hero">
         <div className='filter'></div>
-        <img src="https://res.cloudinary.com/dvdmz9djk/image/upload/v1711316254/JOYAT/images/qdcg6ajhu7szffqtzauu.jpg" alt="" />
+        <img src="https://res.cloudinary.com/dvdmz9djk/image/upload/v1711558391/JOYAT/images/vgjnh5kfbvre9pysgpmp.jpg" alt="" />
         <div className='hero__content'>
             <p className='hero__caption'>Your dream home awaits discover it with us.</p>
             <form className='search-box'>
