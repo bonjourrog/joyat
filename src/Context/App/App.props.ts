0d38985f7383c59ec701bcd32e108212eaf48f5d
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-04-16 12:20:50 -0700

    feat add new props into Context props interface

--- src/Context/App/App.props.ts
+++ src/Context/App/App.props.ts
@@ -1,9 +1,12 @@
 import { ReactNode, SetStateAction } from "react";
+import { City } from "../../types/City";
 
 export interface AppProviderProps{
     children: ReactNode
 }
 export interface AppContextProps{
     selectedCity: string;
-    setSelectedCity: React.Dispatch<SetStateAction<string>>
+    setSelectedCity: React.Dispatch<SetStateAction<string>>;
+    lots: City[];
+    setLots: React.Dispatch<SetStateAction<City[]>>;
 }
