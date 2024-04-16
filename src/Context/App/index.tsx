523e01005ef7718de704aa0635d07fab094a843b
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-04-16 12:21:59 -0700

    feat: add new properties into Appcontext

--- src/Context/App/index.tsx
+++ src/Context/App/index.tsx
@@ -1,13 +1,17 @@
 import { createContext, useState } from "react";
 import { AppContextProps, AppProviderProps } from "./App.props";
+import { City } from "../../types/City";
 
 export const AppContext = createContext<AppContextProps>({}as AppContextProps);
 
 export const AppProvider:React.FC<AppProviderProps> = ({children})=>{
     const [selectedCity, setSelectedCity] = useState<string>("");
+    const [lots, setLots] = useState<City[]>([]);
     return <AppContext.Provider value={{
         selectedCity,
-        setSelectedCity
+        setSelectedCity,
+        lots, 
+        setLots
     }}>
         {children}
     </AppContext.Provider>
