fc9aa636fed155d0870725ec40aa45fd87c8d5ab
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-04-16 12:23:49 -0700

    fix: refactor Map component, set to true draggin, scrollWhrnZoom and touchZoom properties

--- src/Components/Map/index.tsx
+++ src/Components/Map/index.tsx
@@ -8,15 +8,14 @@ const Map: React.FC<MapProps> = ({position, zoom, markers})=>{
         iconUrl:"https://res.cloudinary.com/dvdmz9djk/image/upload/v1711823561/JOYAT/images/icons/zds3mg4cssnj2nzqoemz.png",
         iconSize: [30, 30],
     });
-
-    return <MapContainer className="map" center={ position } zoom={zoom} scrollWheelZoom={false} zoomControl={false} touchZoom={false} dragging={false} doubleClickZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-                />
-                {
-                    markers.length>0? markers.map((elem, index)=>(<Marker key={index} position={elem} icon={newIcon}></Marker>)): null
-                }
-            </MapContainer>
+    return <MapContainer className="map" center={ position } zoom={zoom} scrollWheelZoom={true} zoomControl={false} touchZoom={true} dragging={true} doubleClickZoom={false}>
+        <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+        />
+        {
+            markers.length>0? markers.map((elem, index)=>(<Marker key={index} position={elem} icon={newIcon}></Marker>)): null
+        }
+    </MapContainer>
 }
 export default Map;
