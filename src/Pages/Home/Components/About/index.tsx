831c98273a0e6ab80c700c226ebca28ff39d037b
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-08-07 16:58:03 -0700

    refactor: change image link from About component

--- src/Pages/Home/Components/About/index.tsx
+++ src/Pages/Home/Components/About/index.tsx
@@ -1,5 +1,4 @@
 import './About.css';
-
 const About = ()=>{
     return <div className='about'>
         <div className='about__caption'>
@@ -7,7 +6,7 @@ const About = ()=>{
             <p>En Joyat, nos dedicamos a ofrecer soluciones integrales en el mercado inmobiliario, conectando personas con sus espacios ideales y ayudando a convertir sueños en realidad. Con una trayectoria sólida y un equipo altamente capacitado, nos destacamos por nuestra pasión por el servicio al cliente, la excelencia en la gestión y el compromiso con la satisfacción del cliente.</p>
         </div>
         <div className='bg__image'>
-            <img src="https://res.cloudinary.com/dvdmz9djk/image/upload/v1711558392/JOYAT/images/qimmbwwgceddp27t8amu.jpg" alt=""/>
+            <img src='https://res.cloudinary.com/dvdmz9djk/image/upload/v1723074511/JOYAT/images/jolla/kww4jjfma6xuzrgh3zd8.png' alt=""/>
         </div>
     </div>
 }
