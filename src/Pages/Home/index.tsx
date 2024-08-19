ce7aee887a83d724915c683aaaa997956c3d405a
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-08-19 16:07:26 -0700

    refactor: implementfeature data mock and iterate to mount feature components

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import Feature from '../../Components/Feature';
+import { FeatureProps } from '../../Components/Feature/Feature.props';
 import Footer from '../../Components/Footer';
 import Header from '../../Components/Header';
+import { Feature_DATA_MOCK } from '../../Mocks/Feature';
 import About from './Components/About';
 import Contact from './Components/Contact';
 import Hero from './Components/Hero';
@@ -10,9 +12,8 @@ import './Home.css';
 import { HomeProps } from './Home.props';
 
 const Home: React.FC<HomeProps> = ()=>{
-    
-    const laJollaCaption:string = 'Descubre la oportunidad de tu vida en La Jolla. Terrenos en venta en una ubicación privilegiada, ideales para construir tu hogar o inversión. ¡Haz de este paraíso tu nuevo lugar!'
-    const seaPointCaption:string = 'Construye el futuro en Seapoint. Terrenos en venta en una ubicación exclusiva, diseñados para quienes buscan lo mejor'
+
+    const features: FeatureProps[] = Feature_DATA_MOCK;
 
     return <div className='home'>
         <div className='home__header'>
@@ -21,8 +22,7 @@ const Home: React.FC<HomeProps> = ()=>{
         <Hero/>
         <About/>
         <section className='home__feature'>
-            <Feature url='https://firebasestorage.googleapis.com/v0/b/jojat-4c839.appspot.com/o/jolla%2Fvideos%2F0807.mp4?alt=media&token=4c5e0304-c11d-45c1-b977-de1058b5a609' headline='LA JOLLA' caption={laJollaCaption}/>
-            <Feature url='https://firebasestorage.googleapis.com/v0/b/jojat-4c839.appspot.com/o/jolla%2Fvideos%2Fseapint.mp4?alt=media&token=c9e3f987-555c-4a49-a5bb-64bc84ee5d67' headline='SEAPOINT' caption={seaPointCaption} flip={true}/>
+            {features.map(feature=><Feature url={feature.url} headline={feature.headline} caption={feature.caption} id={feature.id} flip={feature.flip}/>)} 
         </section>
         <Contact/>
         <Location/>
