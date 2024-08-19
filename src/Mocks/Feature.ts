03dece065ae6674d9737b682e937efdd7ec8efa5
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-08-19 15:56:20 -0700

    feat: create feature data mock

---
+++ src/Mocks/Feature.ts
@@ -0,0 +1,18 @@
+import { FeatureProps } from "../Components/Feature/Feature.props";
+
+export const Feature_DATA_MOCK:FeatureProps[]=[
+    {
+        id:'VbKnoZHm0grzQ3flwbDR',
+        url:'https://firebasestorage.googleapis.com/v0/b/jojat-4c839.appspot.com/o/jolla%2Fvideos%2F0807.mp4?alt=media&token=4c5e0304-c11d-45c1-b977-de1058b5a609',
+        caption:'Descubre la oportunidad de tu vida en La Jolla. Terrenos en venta en una ubicación privilegiada, ideales para construir tu hogar o inversión. ¡Haz de este paraíso tu nuevo lugar!',
+        headline:'LA JOLLA',
+        flip:false
+    },
+    {
+        id:'ab1fA14XUwlYPP1Jm1fg',
+        url:'https://firebasestorage.googleapis.com/v0/b/jojat-4c839.appspot.com/o/jolla%2Fvideos%2Fseapint.mp4?alt=media&token=c9e3f987-555c-4a49-a5bb-64bc84ee5d67',
+        caption:'Construye el futuro en Seapoint. Terrenos en venta en una ubicación exclusiva, diseñados para quienes buscan lo mejor',
+        headline:'SEAPOINT',
+        flip:true
+    }
+]
