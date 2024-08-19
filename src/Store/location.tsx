aaafd5236d348411d64ff8c119455274ea7067a9
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-08-19 16:06:06 -0700

    feat: add id and setId properties in location storage

--- src/Store/location.tsx
+++ src/Store/location.tsx
@@ -2,10 +2,14 @@ import { create } from "zustand";
 
 interface LocationStore{
     video:string;
+    id:string;
+    setId: (id:string)=>void;
     setVideo: (video:string)=>void
 }
 
 export const useLocationStore = create<LocationStore>(set=>({
     video:'',
-    setVideo:(video:string)=>{set({video:video})}
+    setVideo:(video:string)=>{set({video:video})},
+    id:'',
+    setId: (id:string)=>{set({id})}
 }))
